58e95a3c26f5cc4757c3279de38c00b056e08346
gytjr4926 <gytjr4926@naver.com>
2024-03-19 09:30:04 +0900

    0319_1

--- 0319_5906900.c
+++ 0319_5906900.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int fac(num){
-    if(num <= 1)    return 1;
-    else return (num*fac(num-1));
-}
+int fac(int num);
 
 int main(void){
     printf("%d",fac(5));
 
     return 0;
+}
+
+
+int fac(num){
+    if(num <= 1)    return 1;
+    else return (num*fac(num-1));
 }
